987e809b4b3bc07964dff4eb5ee07c10c75e7983
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-05 13:14:25 -0400

    Bugs noticed actions wouldnt update or post(didnt properly check crud features cleaned up

--- actions/actionRoutes.js
+++ actions/actionRoutes.js
@@ -1,29 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const actionModel = require("../data/helpers/actionModel");
-const projectModel = require("../data/helpers/projectModel");
+
+
 
 
 router.use(express.json());
  
-function validProject(req, res, next){
-  if(req.body.userId) {
-    projectModel.get().then(allUsers => {
-        let filter = allUsers.filter(users => {
-          return users.id == req.body.userId
-        })
-        if(filter[0]) {
-          next();
-        } else {
-          res.status(401).json({message: "The included userId is not valid. Please include a vallid userId and try again."})
-        }
-      }).catch(err => {
-        res.status(500).json({message: "There was an error validating userId"})
-      })
-  } else {
-      res.status(400).json({message: "Please include a userId in your request"})
-  }
-}
+
 
 router.get('/', (req, res) => {
   actionModel.get().then(allPosts => {
@@ -43,26 +27,21 @@ router.get('/:id', (req, res) => {
 })
 });
 
-router.post('/', validProject, (req, res) => {
-  if (!req.body.text) {
-    res.json(400).json({message: "Please add text and resubmit request"})
-  } else if (req.body.id) {
-    res.status(400).json({message: 'Please remove id and submit again. A new Id will be added automatically.'})
-  } else {
-    actionModel.insert(req.body)
-      .then(postId => {
-        res.status(200).json(postId)
-      })
-      .catch(err => {
-        res.status(500).json({message: "Error while posting"})
-      })
-  }
+router.post('/', (req, res) => {
+    let action = req.body;
+    actionModel.insert(action)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({ message: `There was a problem creating an action.` })
+        });
 });
 
 router.put('/:id', (req, res) => {
   actionModel.update(req.params.id, req.body)
-  .then(count => {
-    res.status(200).json(actions)
+  .then(action => {
+    res.status(200).json(action)
   })
   .catch(err => {
     res.status(500).json({message: "error while updating"})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 const port = 9000;
+const actions = require("./data/helpers/actionModel");
 
 const projectModel = require("./projects/projectRoutes.js");
 const actionModel = require("./actions/actionRoutes.js");
